c50cf41040e973b395ccc1a425f34be74edd9f41
stepanvanecek <stepanvanecek@gmail.com>
2022-11-11 17:39:13 +0100

    calling c15 executable -- make it position-independent, and always call the c15 in the same directory as mt4g, also fallback if not found

--- src/const15Latency.cuh
+++ src/const15Latency.cuh
@@ -6,6 +6,7 @@
 #define CUDATEST_CONST15LATENCY_CUH
 
 #include "GPU_resources.cuh"
+#include <filesystem>
 
 // Call external helper exe for Constant L1.5 Latency due to Constant Memory limit of 64 KiB
 LatencyTuple getC15Latency(int deviceID) {
@@ -16,9 +17,22 @@ LatencyTuple getC15Latency(int deviceID) {
     fprintf(out, "Executing helper c15\n");
 #endif //IsDebug
 #ifdef _WIN32
+    //TODO test this
+    std::cout << "Executing c15.exe - if it does not exist in the current directory, it may crash. " << std::endl;
     snprintf(cmd, 1024, "c15.exe -d:%d", deviceID);
 #else
-    snprintf(cmd, 1024, "./c15 -d:%d", deviceID);
+    std::filesystem::path path = std::filesystem::canonical("/proc/self/exe").parent_path();
+    std::filesystem::path file("c15");
+    path = path / file;
+    if(std::filesystem::exists(path))
+    {
+        std::cerr << "Epath " << path << "does not exist. Skipping..." << std::endl;
+        snprintf(cmd, 1024, "%s -d:%d", path.u8string().c_str(), deviceID);
+    }
+    else
+    {
+        std::cerr << "ERROR: path " << path << "does not exist. Skipping..." << std::endl;
+    }
 #endif
 
     FILE *p;
